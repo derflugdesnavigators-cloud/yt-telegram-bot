fad04ef8621feb5ef072548357ca341967267ab5
derflugdesnavigators-cloud <derflugdesnavigators@gmail.com>
2026-03-28 21:10:24 +0100

    Update check.py

--- check.py
+++ check.py
@@ -5,7 +5,7 @@ import feedparser
 RSS = "https://www.youtube.com/feeds/videos.xml?channel_id=UCshyfrLaCobJZJzhEnvMwtg"
 CHAT_ID = "-1003710777199"
 
-TOKEN = os.environ["8670459256:AAEdA6Ne9qzVj2I4gTNwYCYqYkzC3Wt2XVU"]
+TOKEN = os.environ["TOKEN"]
 
 feed = feedparser.parse(RSS)
 entry = feed.entries[0]
@@ -13,7 +13,7 @@ entry = feed.entries[0]
 title = entry.title
 link = entry.link
 
-text = f"🚀 New Video or Live Stream!\n\n{title}\n\n{link}"
+text = f"🚀 New Video or Live Stream! \n\n{title}\n\n{link}"
 
 send = requests.get(
     f"https://api.telegram.org/bot{TOKEN}/sendMessage",
